2febbd56802ce10da347c14c252c16b6a9914800
Masahiro Nagano <kazeburo@gmail.com>
2012-09-27 12:38:10 +0900

    initial commit

---
+++ lib/DBIx/DSN/Resolver.pm
@@ -0,0 +1,32 @@
+package DBIx::DSN::Resolver;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+DBIx::DSN::Resolver -
+
+=head1 SYNOPSIS
+
+  use DBIx::DSN::Resolver;
+
+=head1 DESCRIPTION
+
+DBIx::DSN::Resolver is
+
+=head1 AUTHOR
+
+Masahiro Nagano E<lt>kazeburo {at} gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
